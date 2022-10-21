e6241b0e8e98e7bae864635d6bdbc6ffc678d8bf
Chizoba8 <99368906+Chizoba8@users.noreply.github.com>
2022-10-21 12:03:18 +0100

    devise commit

---
+++ 0x01-challenge/blog/config/initializers/devise.rb
@@ -0,0 +1,9 @@
+# Use this hook to configure devise mailer, warden hooks and so forth.
+# Many of these configuration options can be set straight in your model.
+Devise.setup do |config|
+  # The secret key used by Devise. Devise uses this key to generate
+  # random tokens. Changing this key will render invalid all existing
+  # confirmation, reset password and unlock tokens in the database.
+  # Devise will use the `secret_key_base` as its `secret_key`
+  # by default. You can change it below and use your own secret key.
+  # config.secret_key = 'b3abc526ab533cd65c61f621629e79effaa1742b3a3a6c7af1ecd5cfa4d50bfb75e3d8f7fabdceda07f0f554840bf4936c56f33c5e2d64a6e5371e1b54749013'
